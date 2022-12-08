466c0455b5edf8e6cec51ff2af4269c6d24d8c21
CJINBNH <alex_egorov_89@mail.ru>
2022-12-08 20:25:27 +0400

    seminar 2

---
+++ lesson2/Task2.java
@@ -0,0 +1,24 @@
+// Напишите метод, который сжимает строку
+// Пример: aaaabbbccccc --> a4b3c5
+
+public class Task2 {
+
+    private static String compress(String str) {
+        int count = 1;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i < str.length(); i++)
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                count++;
+            } else {
+                builder.append(Character.toString(str.charAt(i - 1)) + count);
+                count = 1;
+            }
+        builder.append(Character.toString(str.charAt(str.length() - 1)) + count);
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "aaaabbbcdd";
+        System.out.println(compress(str));
+    }
+}
