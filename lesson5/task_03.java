c113b9f89027f01ba38e3fa80eaa07513d05d0f5
CJINBNH <alex_egorov_89@mail.ru>
2022-12-22 17:58:09 +0400

    Задачи семинара №5

---
+++ lesson5/task_03.java
@@ -0,0 +1,8 @@
+// Написать метод, который переводит число из римского формата записи в арабский. 
+// Например, MMXXII = 2022
+
+public class task_03 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ lesson5/task_04.java
@@ -0,0 +1,7 @@
+// Написать метод, который переведёт данное целое число в римский формат.
+
+public class task_04 {
+    public static void main(String[] args) {
+        
+    }
+}
